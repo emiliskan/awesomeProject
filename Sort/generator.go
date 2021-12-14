8710c40bd6bf95cd6639696656bbfefa006aabc1
Emil <emilagamamedov@gmail.com>
2021-12-14 15:57:15 +0300

    speeding up

--- Sort/generator.go
+++ Sort/generator.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"math/rand"
 	"os"
 	"strconv"
@@ -9,44 +10,47 @@ import (
 )
 
 const MaxNum int = 999_999_999
-const ChunkSize int = 10_000_000
+const ChunkSize int = 10_000
 
 func generatePhoneFiles() {
-	num := 1_000_000_000
+	num := 79_000_000_000
 
 	phonesFile, _ := os.OpenFile("phones", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
-	var phonesChunk []string
+	var phonesChunk [ChunkSize]string
 
+	chunkCount := 1
+	chunkPos := 0
 	dataWriter := bufio.NewWriter(phonesFile)
 
 	for i := 0; i < MaxNum; i++ {
 
-		phone := "+79" + strconv.Itoa(num)[1:]
-		phonesChunk = append(phonesChunk, phone)
-
-		if len(phonesChunk) > ChunkSize {
+		if i/chunkCount >= ChunkSize {
 			writeChunkToFile(dataWriter, phonesChunk)
-			phonesChunk = nil
+			chunkCount++
+			chunkPos = 0
 		}
 
+		phone := "+" + strconv.Itoa(num)
+		phonesChunk[chunkPos] = phone
 		num++
+		chunkPos++
 	}
 
 	writeChunkToFile(dataWriter, phonesChunk)
 
 	err := dataWriter.Flush()
 	if err != nil {
-		return
+		fmt.Print(err)
 	}
 	phonesFile.Close()
 
 }
 
-func writeChunkToFile(dataWriter *bufio.Writer, chunk []string) {
+func writeChunkToFile(dataWriter *bufio.Writer, chunk [ChunkSize]string) {
 	rand.Shuffle(len(chunk), func(i, j int) {
 		chunk[i], chunk[j] = chunk[j], chunk[i]
 	})
-	data := strings.Join(chunk, "\n")
+	data := strings.Join(chunk[:], "\n")
 	_, _ = dataWriter.WriteString(data + "\n")
 }
